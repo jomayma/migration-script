38a6886b5cd9809e8b97baa7e1a604892bba46d7
Jorge Mayo Martín <jmayomartin@gmail.com>
2018-03-27 22:18:11 +0200

    Lapsed execution added.

--- test_script.sh
+++ test_script.sh
@@ -1,10 +1,13 @@
 ./import_all.sh
+START="$(date +%s)"
 node migrate-data.js
+END="$(date +%s)"
+DIFF="$(($END-$START))"
 CHK=`mongo --quiet edx-course-db query_checker.js`
 RES='{ "state" : "Alabama" }'
 
 if [ "$CHK" == "$RES" ]; then
-  echo "MIGRATION SCRIPT OK"
+  echo "MIGRATION SCRIPT OK. EXECUTED ON $DIFF SECONDS"
 else
   echo "MIGRATION SCRIPT KO"
 fi
